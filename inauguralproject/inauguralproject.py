b738ab7efd7a990f456e712ab5f18712a63b753a
Tml710 <tml710@alumni.ku.dk>
2023-05-22 20:46:51 +0200

    aaaa

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -232,3 +232,146 @@ class HouseholdSpecializationModelClass:
             print(f"Residual value: {solution.fun:.2f}")
             print(f"Success: {solution.success}")
             print(f"Message: {solution.message}")
+
+    def calc_utility5(self, LM, HM, LF, HF):
+        """ calculate utility with an inequality aversion component """
+
+        par = self.par
+        sol = self.sol
+
+        # a. consumption of market goods
+        C = par.wM * LM + par.wF * LF
+
+        # b. home production
+        if par.sigma == 0:
+            H = optimize.minimize(HM, HF)
+        elif par.sigma == 1:
+            H = HM**(1 - par.alpha) * HF**par.alpha
+        else:
+            H = ((1 - par.alpha) * HM**((par.sigma - 1) / par.sigma) + par.alpha * HF**((par.sigma - 1) / par.sigma))**(par.sigma / (par.sigma - 1))
+
+        # c. total consumption utility
+        Q = C**par.omega * H**(1 - par.omega)
+        utility = np.fmax(Q, 1e-8)**(1 - par.rho) / (1 - par.rho)
+
+        # d. disutility of work
+        epsilon_ = 1 + 1 / par.epsilon
+        TM = LM + HM
+        TF = LF + HF
+        disutility = par.nu * (TM**epsilon_ / epsilon_ + TF**epsilon_ / epsilon_)
+
+        # e. inequality aversion component
+        inequality_aversion = par.lambda_ineq * (HF - HM)**2  # Adjust the parameter lambda_ineq as desired
+
+        return utility - disutility - inequality_aversion
+    
+    def estimate5(self, do_print=False):
+        par = self.par
+        sol = self.sol
+
+        # Initial guess and bounds for the optimization
+        x0 = [0.5, 0.1]
+        bounds = ((0, 1), (0, 1))
+
+        # Define the target function for optimization
+        def target(x):
+            par.alpha, par.sigma = x
+            self.solve_wF_vec5()
+            self.run_regression()
+            residual = (sol.beta0 - par.beta0_target) ** 2 + (sol.beta1 - par.beta1_target) ** 2
+            return residual
+
+        # Perform the optimization
+        solution = optimize.minimize(target, x0, method='Nelder-Mead', bounds=bounds)
+        sol.alpha, sol.sigma = solution.x
+
+        if do_print:
+            # Print the optimization results
+            print("Optimization results:")
+            print("=====================")
+            print(f"Alpha: {sol.alpha:.2f}")
+            print(f"Sigma: {sol.sigma:.2f}")
+            print(f"Residual value: {solution.fun:.2f}")
+            print(f"Success: {solution.success}")
+            print(f"Message: {solution.message}")
+
+    def solve_wF_vec5(self, discrete=False):
+        """ solve model for vector of female wages """
+        par = self.par
+        sol = self.sol
+
+        HF_HM_ratios = np.zeros(par.wF_vec.size)
+
+        for i, wF in enumerate(par.wF_vec):
+
+            # update female wage
+            par.wF = wF
+
+            # solve the model
+            if discrete:
+                opt = self.solve_discrete()
+            else:
+                opt = self.solve5()
+
+            # store the results
+            sol.LM_vec[i] = opt.LM
+            sol.HM_vec[i] = opt.HM
+            sol.LF_vec[i] = opt.LF
+            sol.HF_vec[i] = opt.HF
+
+            # calculate HF/HM ratio
+            sol.HM = opt.HM
+            sol.HF = opt.HF
+            HF_HM_ratios[i] = sol.HF / sol.HM
+
+        # store HF/HM ratios in solution object
+        sol.HF_HM_ratios = HF_HM_ratios
+
+        # run regression if not discrete
+        if not discrete:
+            self.run_regression()
+
+    def solve5(self, do_print=False):
+        """Solve model continuously.
+
+        Args:
+            do_print (bool): Whether to print the results (default is False).
+
+        Returns:
+            An object with the optimal choices of LM, HM, LF, and HF.
+
+        """
+        # Get model parameters and solution
+        par = self.par
+        sol = self.sol
+
+        # Define a function to minimize (negative of utility)
+        def neg_utility(choices):
+            LM, HM, LF, HF = choices
+            return -self.calc_utility5(LM, HM, LF, HF)
+
+        # Set initial guesses and bounds for the optimization problem
+        x0 = [2, 2, 2, 2]
+        bounds = [(0, 24), (0, 24), (0, 24), (0, 24)]
+
+        # Solve the optimization problem
+        opt_result = optimize.minimize(neg_utility, x0, bounds=bounds, method='Nelder-Mead')
+        opt = SimpleNamespace(
+            LM=opt_result.x[0],
+            HM=opt_result.x[1],
+            LF=opt_result.x[2],
+            HF=opt_result.x[3],
+            )
+
+        # Save the optimal choices to the solution object
+        sol.LM = opt.LM
+        sol.HM = opt.HM
+        sol.LF = opt.LF
+        sol.HF = opt.HF
+
+        # Print the results if requested
+        if do_print:
+            for k, v in opt.__dict__.items():
+                print(f"{k} = {v:6.4f}")
+
+        return opt
